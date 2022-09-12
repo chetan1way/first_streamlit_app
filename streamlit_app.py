3609f1290883b13c6eb79796dd9e3d1ffcbe1ace
chetan1way <90516573+chetan1way@users.noreply.github.com>
2022-09-12 20:41:03 +0530

    Update streamlit_app.py
    Added Smileys to the text

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
 
-streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Bluleberry Oatmeal')
-streamlit.text('Kale, Spinich & Rocket Smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.header('🥣 Breakfast Favorites')
+streamlit.text('🥗 Omega 3 & Bluleberry Oatmeal')
+streamlit.text('🐔 Kale, Spinich & Rocket Smoothie')
+streamlit.text('🥑🍞 Hard-Boiled Free-Range Egg')
