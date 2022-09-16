694d7623924529a21d70e8178ab04559b8b46fd5
chetan1way <90516573+chetan1way@users.noreply.github.com>
2022-09-16 11:31:57 +0530

    Update streamlit_app.py
    another function for snowflake

--- streamlit_app.py
+++ streamlit_app.py
@@ -56,13 +56,17 @@ except URLError as e:
 streamlit.stop()
 
 ## Snowflake Connection and usage starts from here
-
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("SELECT * from fruit_load_list")
-my_data_rows = my_cur.fetchall()
 streamlit.header("The Fruit Load Contains:")
-streamlit.dataframe(my_data_rows)
+def get fruit_load_list():
+  with my_cnx.cursor() as my_cur:
+    my_cur.execute("SELECT * from fruit_load_list")
+    return my_cur.fetchall()
+
+# add button
+if streamlit.button('Get Fruit Load List'):
+  my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+  my_data_rows = get fruit_load_list()
+  streamlit.dataframe(my_data_rows)
 
 
 ## Second Choice box
